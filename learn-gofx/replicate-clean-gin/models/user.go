6e51bd0f3d9efc37bce7a47088f27d48b306f83b
Ujjwol Kayastha <uzol123@gmail.com>
2021-02-18 17:27:48 +0545

    feat: add user model

---
+++ learn-gofx/replicate-clean-gin/models/user.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        uint      `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (u User) TableName() string {
+	return "users"
+}
